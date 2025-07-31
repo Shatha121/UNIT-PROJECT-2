59df814428bafe286c6cae18206ca3ee5b5284d1
shatha <shatha25200@gmail.com>
2025-07-31 10:28:49 +0300

    Fixed the security

--- safenet/safenest/settings.py
+++ safenet/safenest/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-a&#_e-@pm!u%hbu*f+mvie4joife!ri!2%#2e86uuou6d=#5xf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
